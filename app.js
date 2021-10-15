0de5814765d55e1d5db83bd98fecf578a4f71647
Mohammad Ali Al Maduri <ali.mdrw@gmail.com>
2021-10-16 07:02:32 +0800

    Add mac addresses

--- app.js
+++ app.js
@@ -16,6 +16,66 @@ const dbName = process.env.DB_NAME;
 
 mongoose.connect(`mongodb+srv://${dbUser}:${dbPass}@cluster0.znyyn.mongodb.net/${dbName}`);
 
+const macList = [
+  "0c:a8:a7:31:a2:94",
+  "10:89:fb:1d:5f:b8",
+  "14:5e:69:7f:47:e3",
+  "18:02:ae:71:8a:bf",
+  "18:02:ae:78:53:65",
+  "18:02:ae:a0:ce:47",
+  "20:3b:69:8c:34:cd",
+  "20:3b:69:96:50:bd",
+  "20:64:cb:c5:60:e1",
+  "20:74:54:9b:97:99",
+  "20:74:54:a2:96:17",
+  "20:74:54:c0:b4:bd",
+  "28:31:66:70:34:4f",
+  "28:31:66:73:de:63",
+  "28:31:66:80:78:35",
+  "28:31:66:84:02:fd",
+  "2c:d0:66:09:0f:94",
+  "2c:d0:66:1d:4e:86",
+  "2c:ff:ee:9d:aa:c3",
+  "30:94:35:66:a8:15",
+  "30:af:ce:09:87:fb",
+  "30:af:ce:23:ff:65",
+  "34:e9:11:12:c5:47",
+  "34:e9:11:33:e6:41",
+  "3c:a6:16:ec:6a:b3",
+  "3c:b6:b7:39:86:f9",
+  "5c:66:6c:cf:c4:af",
+  "68:bf:c4:12:c1:6e",
+  "68:bf:c4:ef:cd:5c",
+  "6c:24:a6:d6:64:ff",
+  "6c:24:a6:e6:fa:13",
+  "70:78:8b:b1:08:71",
+  "70:b7:aa:ba:11:77",
+  "70:b7:aa:c2:ca:75",
+  "7c:2a:db:c2:47:b8",
+  "98:c8:b8:87:ed:b1",
+  "9c:6b:72:69:d3:07",
+  "a0:22:de:12:00:47",
+  "a4:d9:90:72:55:d7",
+  "b8:c7:4a:a5:9c:eb",
+  "c0:2e:25:a5:d2:91",
+  "c4:e3:9f:d0:33:59",
+  "c8:16:da:1a:5a:1f",
+  "c8:16:da:51:6a:dd",
+  "d0:28:ba:11:4b:d7",
+  "e0:1f:88:57:bf:cc",
+  "f8:e7:a0:0e:c5:ed",
+  "f8:e7:a0:60:f4:93",
+  "14:5e:69:a0:32:15",
+  "2c:d0:66:1e:de:dc",
+  "a4:d9:90:41:4d:ef",
+  "c0:87:eb:b9:7c:41",
+  "60:30:d4:b9:68:22",
+  "c8:f2:30:09:44:d4",
+  "98:c8:b8:9c:cb:97",
+  "f0:6d:78:3e:6d:e7",
+  "d8:1e:dd:40:bc:5b"
+]
+
 const macSchema = {
   mac_address: String,
   detail: String
@@ -23,3 +83,35 @@ const macSchema = {
 
 const Mac = mongoose.model('Mac', macSchema)
 
+const macResult = []
+
+for(const mac of macList) {
+  const macAdd = new Mac({
+    mac_address: mac,
+    detail: '-'
+  })
+
+  macResult.push(macAdd)
+}
+
+
+app.get('/', (req, res) => {
+  Mac.find({}, (err, foundItems) => {
+    if(foundItems.length === 0) {
+      Mac.insertMany(macResult, err => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log('Successfully saved default items to Database');
+        }
+      })
+    }
+    res.render("list", { macAddresses: foundItems });
+  })
+})
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
--- public/js/script.js
+++ public/js/script.js
@@ -2,11 +2,11 @@ const toastBody = document.querySelector('div#liveToast div div.toast-body')
 const toastLiveExample = document.getElementById('liveToast')
 
 document.addEventListener('click', evt => {
-  // if(evt.target.dataset.duration) {
+  const textCopied = evt.target.parentElement.dataset.macAddress
+  if (evt.target.parentElement.dataset.macAddress) {
     const toast = new bootstrap.Toast(toastLiveExample)
-    const textCopied = evt.target.innerText
     navigator.clipboard.writeText(textCopied)
     toastBody.innerText = `${textCopied} copied to clipboard`
     toast.show()
-  // }
+  }
 })
