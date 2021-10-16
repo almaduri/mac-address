9ce7caf84a75b2abbcdb70a1ca781b156a4629d4
Mohammad Ali Al Maduri <ali.mdrw@gmail.com>
2021-10-16 08:57:51 +0800

    Change toast delay

--- public/js/script.js
+++ public/js/script.js
@@ -3,8 +3,14 @@ const toastLiveExample = document.getElementById('liveToast')
 
 document.addEventListener('click', evt => {
   const textCopied = evt.target.parentElement.dataset.macAddress
+
+  const options = {
+    animatio: true,
+    delay: 1000
+  }
+
   if (evt.target.parentElement.dataset.macAddress) {
-    const toast = new bootstrap.Toast(toastLiveExample)
+    const toast = new bootstrap.Toast(toastLiveExample, options)
     navigator.clipboard.writeText(textCopied)
     toastBody.innerText = `${textCopied} copied to clipboard`
     toast.show()
